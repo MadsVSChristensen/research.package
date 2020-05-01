718ab20810bf4b7acb149e4838fddd30c68c36a7
Unknown <s174286@student.dtu.dk>
2020-05-01 12:13:09 +0200

    Database enable/disable fix

--- example/lib/survey_page.dart
+++ example/lib/survey_page.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:research_package/research_package.dart';
 
@@ -10,6 +11,7 @@ class SurveyPage extends StatelessWidget {
   String _encode(Object object) =>
       const JsonEncoder.withIndent(' ').convert(object);
 
+
   void printWrapped(String text) {
     final pattern = new RegExp('.{1,800}'); // 800 is the size of each chunk
     pattern.allMatches(text).forEach((match) => print(match.group(0)));
@@ -17,7 +19,10 @@ class SurveyPage extends StatelessWidget {
 
   void resultCallback(RPTaskResult result) async {
     printWrapped(_encode(result));
+    
+    if (await FirebaseAuth.instance.currentUser() != null) {
     await DBService().updateDBData(_encode(result.results));
+    }
     //make sure in future not to save empty results.
   }
 
