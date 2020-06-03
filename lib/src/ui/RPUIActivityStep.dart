bde91bb8b9f4c758df9ee5cf1bf851557267abcc
Mads Christensen <s174238@student.dtu.dk>
2020-06-03 12:23:32 +0200

    qf

--- lib/src/ui/RPUIActivityStep.dart
+++ lib/src/ui/RPUIActivityStep.dart
@@ -12,8 +12,7 @@ class RPUIActivityStep extends StatefulWidget {
   _RPUIActivityStepState createState() => _RPUIActivityStepState();
 }
 
-class _RPUIActivityStepState extends State<RPUIActivityStep>
-    with CanSaveResult {
+class _RPUIActivityStepState extends State<RPUIActivityStep> with CanSaveResult {
   // Dynamic because we don't know what value the RPChoice will have
   dynamic _currentActivityBodyResult;
   bool readyToProceed;
@@ -47,43 +46,35 @@ class _RPUIActivityStepState extends State<RPUIActivityStep>
   Widget stepBody(RPActivityStep activityStep) {
     switch (activityStep.runtimeType) {
       case RPTrailMakingActivity:
-        return RPUITrailMakingActivityBody(activityStep, gestureController,
-            (result) {
+        return RPUITrailMakingActivityBody(activityStep, gestureController, (result) {
           this.currentActivityBodyResult = result;
         });
       case RPTappingActivity:
-        return RPUITappingActivityBody(activityStep, gestureController,
-            (result) {
+        return RPUITappingActivityBody(activityStep, gestureController, (result) {
           this.currentActivityBodyResult = result;
         });
       case RPLetterTappingActivity:
-        return RPUILetterTappingActivityBody(activityStep, gestureController,
-            (result) {
+        return RPUILetterTappingActivityBody(activityStep, gestureController, (result) {
           this.currentActivityBodyResult = result;
         });
       case RPReactionTimeActivity:
-        return RPUIReactionTimeActivityBody(activityStep, gestureController,
-            (result) {
+        return RPUIReactionTimeActivityBody(activityStep, gestureController, (result) {
           this.currentActivityBodyResult = result;
         });
       case RPRapidVisualInfoProcessingActivity:
-        return RPUIRapidVisualInfoProcessingActivityBody(
-            activityStep, gestureController, (result) {
+        return RPUIRapidVisualInfoProcessingActivityBody(activityStep, gestureController, (result) {
           this.currentActivityBodyResult = result;
         });
       case RPPairedAssociatesLearningActivity:
-        return RPUIPairedAssociatesLearningActivityBody(
-            activityStep, gestureController, (result) {
+        return RPUIPairedAssociatesLearningActivityBody(activityStep, gestureController, (result) {
           this.currentActivityBodyResult = result;
         });
       case RPCorsiBlockTappingActivity:
-        return RPUICorsiBlockTappingActivityBody(
-            activityStep, gestureController, (result) {
+        return RPUICorsiBlockTappingActivityBody(activityStep, gestureController, (result) {
           this.currentActivityBodyResult = result;
         });
       case RPStroopEffectActivity:
-        return RPUIStroopEffectActivityBody(activityStep, gestureController,
-            (result) {
+        return RPUIStroopEffectActivityBody(activityStep, gestureController, (result) {
           this.currentActivityBodyResult = result;
         });
       default:
@@ -93,55 +84,11 @@ class _RPUIActivityStepState extends State<RPUIActivityStep>
 
   @override
   Widget build(BuildContext context) {
-    return /*Scaffold(
-      resizeToAvoidBottomInset: true,
-      body: */
-        SafeArea(
+    return SafeArea(
       child: Container(
         child: stepBody(widget.step),
       ),
-    )
-        /*,
-      persistentFooterButtons: <Widget>[
-        FlatButton(
-          onPressed: () => blocTask.sendStatus(StepStatus.Canceled),
-          child: Text(
-            "CANCEL",
-            style: TextStyle(color: Colors.redAccent),
-          ),
-        ),
-        RaisedButton(
-          color: Theme.of(context).accentColor,
-          textColor: Colors.white,
-          child: Text(
-            "NEXT",
-          ),
-          onPressed: readyToProceed
-              ? () {
-                  // Communicating with the RPUITask Widget
-                  blocTask.sendStatus(StepStatus.Finished);
-                  createAndSendResult();
-                }
-              : null,
-        ),
-      ],
-    )*/
-        ;
-  }
-
-  // Render the title above the ActivityBody
-  Widget title() {
-    if (widget.step.title != null) {
-      return Padding(
-        padding: const EdgeInsets.only(bottom: 24, left: 8, right: 8, top: 8),
-        child: Text(
-          widget.step.title,
-          style: RPStyles.h2,
-          textAlign: TextAlign.left,
-        ),
-      );
-    }
-    return null;
+    );
   }
 
   @override
